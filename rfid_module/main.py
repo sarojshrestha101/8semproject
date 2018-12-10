6089046ddec4e18ae2bdf11c41f31ba544a11c1b
Saroj Shrestha <saroj.shr911@gamil.com>
2018-12-10 12:38:02 +0545

    rfid python file added

---
+++ rfid_module/main.py
@@ -0,0 +1,26 @@
+import SimpleMFRC522
+
+import requests
+import datetime
+
+reader = SimpleMFRC522.SimpleMFRC522()
+server = 'htpp://httpbin.org/post'
+while True:
+
+	try:
+		ts = datetime.datetime.utcnow()
+		UID, text = reader.read()
+		print("UID found")
+		print(UID)
+
+		payload = {
+			'UID' : UID,
+			'timeStamp' : ts
+		}
+
+		r = requests.get(server, params = payload)
+		print(r.url)
+	
+	finally:
+		GPIO.cleanup()
+
