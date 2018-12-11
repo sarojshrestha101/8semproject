5c5818dc97d29559d3fd72b0acf3d06f285da302
Saroj SHrestha <saroj.shr911@gmail.com>
2018-12-11 06:00:01 +0000

    mysql way python done

--- rfid_module/main.py
+++ rfid_module/main.py
@@ -5,22 +5,26 @@ import mysql.connector
 from mysql.connector import errorcode
 import time
 
-query = ("INSERT INTO rfid_temp(UID) VALUES(%s);")
 try:
-    cnx = mysql.connector.connect(
-		host = '192.168.100.143:3306',
-        user = 'root',
-        passwd = '',
-        database = 'erashid'
+	cnx = mysql.connector.connect(
+		host = '192.168.100.143',
+        	user = 'root',
+        	passwd = '',
+        	database = 'erashid'
 	)
 	cursor = cnx.cursor()
 	while True:
 		reader = SimpleMFRC522.SimpleMFRC522()
 		UID, text = reader.read()
-		data = UID
-		cursor.execute(query,data)
+		
+		query = "INSERT INTO rfid_temp(UID) VALUE (" + str(UID) + ") "
+		val = (UID)
+		
+		print val
+		print query
+		cursor.execute(query)
 		cnx.commit()
-		sleep(2)
+		time.sleep(2)
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
