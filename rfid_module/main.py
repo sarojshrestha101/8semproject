599204dc7a992a6133d26fbf7b8bcb2789ae31ed
saroj shrestha <-v>
2018-12-10 21:26:36 -0800

    mysql way

--- rfid_module/main.py
+++ rfid_module/main.py
@@ -1,27 +1,27 @@
 import RPi.GPIO as GPIO
 import SimpleMFRC522
 
-import requests
-import datetime
-import time
+import mysql.connector
+from mysql.connector import errorcode
 
-reader = SimpleMFRC522.SimpleMFRC522()
-server = 'http://192.168.100.143/8semproject/index.php'
-while True:
-
-	try:
+query = ("INSERT INTO rfid_temp(UID) VALUES(%s);")
+try:
+    cnx = mysql.connector.connect(
+		host = '192.168.100.143:3306'
+        user = 'root',
+        passwd = '',
+        database = 'erashid'
+	)
+	cursor = cnx.cursor()
+	while True:
+		reader = SimpleMFRC522.SimpleMFRC522()
 		UID, text = reader.read()
-		print("UID found")
-		print(UID)
+		data = UID
+		cursor.execute(query,data)
+		cnx.commit()
 
-		payload = {
-			'UID' : UID
-		}
-
-		r = requests.post(server, params = payload)
-		print(r.url)
-	
-	finally:
-		print("finish")
-		GPIO.cleanup()
-		time.sleep(2)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("some thing is wrong")
+    else:
+            print(err)
